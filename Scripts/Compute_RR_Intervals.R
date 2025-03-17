01236655ba445e3ce973a412a5ce3136bbefbb6b
ManelRF <manelrf5@gmail.com>
2025-03-17 17:43:58 +0100

    Create Compute_RR_Intervals.R
    Created a script that loads the Compute_R_Peaks script in order to find the RR intervals and plot them.

---
+++ Scripts/Compute_RR_Intervals.R
@@ -0,0 +1,8 @@
+source(here("Scripts", "Compute_R_Peaks"))
+
+# Compute the RR intervals
+rr_intervals <- diff(r_peaks$Time)
+
+# Plot the RR intervals
+plot(rr_intervals, type="l", col="red", xlab="Beat",
+     ylab="RR Interval")
